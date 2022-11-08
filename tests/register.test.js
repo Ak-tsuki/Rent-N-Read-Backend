656df6265767fa3a3c07a8d3c701cf7e15d0d97a
tsering-hub <ts19sherpa@gmail.com>
2022-11-08 20:40:44 +0545

    TDD testing of register without email

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,15 +1,36 @@
 const request = require("supertest");
 const app = require("../app");
 
+// Creating new user without email
+test("Sign up testing for a new user without email", async () => {
+  await request(app)
+    .post("/user/register")
+    .send({
+      username: "tsering",
+      contact_no: "988734345",
+      password: "1234556",
+    })
+    .expect("Content-Type", /json/)
+    .expect(201)
+    .then((res) => {
+      expect.arrayContaining([
+        expect.objectContaining({
+          msg: "User Registered Successfully",
+          success: true,
+        }),
+      ]);
+    });
+});
+
 // Creating new user
 test("Sign up testing for a new user", async () => {
   await request(app)
     .post("/user/register")
     .send({
-      username: "username",
-      email: "email",
-      contact_no: "contact_no",
-      password: "hashed_pw",
+      username: "tsering",
+      email: "ts12@gmail.com",
+      contact_no: "988734345",
+      password: "1234556",
     })
     .expect("Content-Type", /json/)
     .expect(201)
@@ -28,10 +49,10 @@ test("Sign up testing for a exiting user", async () => {
   await request(app)
     .post("/user/register")
     .send({
-      username: "username",
-      email: "email",
-      contact_no: "contact_no",
-      password: "hashed_pw",
+      username: "tsering",
+      email: "ts12@gmail.com",
+      contact_no: "988734345",
+      password: "1234556",
     })
     .expect("Content-Type", /json/)
     .expect(200)
@@ -53,7 +74,7 @@ test("Sign up testing for a user not in Json Format", async () => {
       "username: tsering",
       "email: ts12@gmail.com",
       "contact_no: 977342232",
-      "password: 1234"
+      "password: 12344342"
     )
     .expect("Content-Type", /json/)
     .expect(400)
