76693b953b26c64018e37a9406b3d1f13d13d447
aayush <aayush172@gmail.com>
2022-12-08 16:39:44 +0545

    recommendation with category

--- routers/recommendationRouter.js
+++ routers/recommendationRouter.js
@@ -8,25 +8,31 @@ const auth = require("../middleware/auth");
 const uploadFile = require("../file/uploadFile");
 const Rent = require("../models/rentModel");
 
-
-
 // recommedation book by category
-router.get("/book/recommendation",auth.userGuard, (req, res) => {
-    Rent.findOne({userId:req.userInfo._id}).sort({ _id: -1 })
-      .then((rent) => {
-        if (rent != null) {
-            res.status(200).json({
+router.get("/book/recommendation", auth.userGuard, (req, res) => {
+  Rent.findOne({ userId: req.userInfo._id })
+    .sort({ _id: -1 })
+    .then((rent) => {
+      if (rent != null) {
+        Book.findOne({ _id: rent.bookId })
+        .then((mainbook) => {
+            console.log(mainbook.category);
+          Book.find({ category: { $in: mainbook.category } }).then(
+            (book) => {
+              res.status(201).json({
                 success: true,
                 data: book,
               });
-        }
-      })
-      .catch((e) => {
-        res.status(400).json({
-          msg: e,
+            }
+          );
         });
+      }
+    })
+    .catch((e) => {
+      res.status(400).json({
+        msg: e,
       });
-  });
-  
+    });
+});
 
-  module.exports = router;
+module.exports = router;
