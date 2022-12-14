f49922b6c42619179a28671f8466b37a6f0a35b2
Rijwol Shakya <shakyarijwol09@gmail.com>
2022-12-14 14:04:27 +0545

    Rent EBook Model Made

---
+++ models/rentEbookModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { default: isBoolean } = require("validator/lib/isboolean");
+
+//Rent Book Model Collection Create
+const RentEbook = new mongoose.Schema(
+  {
+    ebookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "EBook",
+      required: true,
+    },
+    bookOwner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    start_date: {
+      type: String,
+    },
+    end_date: {
+      type: String,
+    },
+    no_of_days: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    total_price: {
+      type: Number,
+      required: true,
+    },
+    rent_status: {
+      type: String,
+      default: "Pending",
+    },
+    payment_method: {
+      type: String,
+    },
+    payment_status: {
+      type: String,
+      default: "Pending",
+    },
+    contact_no: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("RentEbook", RentEbook);
+e
