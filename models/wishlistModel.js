4c8deb1ed71df8165ba4e900c5ad9df1adc7f528
Rijwol Shakya <shakyarijwol09@gmail.com>
2022-12-19 11:38:45 +0545

    Wishlist Model Made

---
+++ models/wishlistModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Wishlist = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Wishlist", Wishlist);
