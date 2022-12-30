e7a79c3e8cb88d96aa40ddd00692c66529078cdd
aaronthaku <me9aaron@gmail.com>
2022-12-30 18:06:38 +0545

    changes in review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,30 +1,35 @@
 const mongoose = require("mongoose");
 
-const Review = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const Review = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    bookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+    ebookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ebook",
+    },
+    audioBookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AudioBook",
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    review: {
+      type: String,
+      required: true,
+    },
   },
-  bookId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Book",
-  },
-  ebookId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Ebook",
-  },
-  audioBookId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "AudioBook",
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  review: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model("Review", Review);
